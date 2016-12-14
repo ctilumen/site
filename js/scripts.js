3b0cf9ab2452da43b12342ab7a1120b9e40da5d2
Lincon Keiti Kusunoki <linconkusunoki@gmail.com>
2016-12-14 18:27:49 +0000

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+(function($){
+    
+    $('document').ready(function(){
+    
+        $('.home-slider ul').slick({
+            arrows: false,
+            dots: true,
+            autoplay: true,
+            lazyLoad: 'ondemand'
+        });
+        
+    });
+    
+})(jQuery);
