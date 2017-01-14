0d956168c3dc0900d86c0824d29a6b7e06c9f49d
Lincon Keiti Kusunoki <linconkusunoki@gmail.com>
2017-01-14 13:52:57 +0000

    alteracoes

---
+++ _posts/2017-01-14-exemplo-site-novo.md
@@ -0,0 +1,35 @@
+---
+layout: post
+---
+
+_sublinhado_
+
+# titulo
+
+## titulo
+
+* lista
+* lista
+* aaeaepáe
+* aeklpoaekopae
+* oakepaeo
+* 
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+
+
+[http://google.com google][a]
+
+```html
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Hello, World!</p>
+  </body>
+</html>
+```
+
+
+![My helpful screenshot](/img/posts/post1.jpg)
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,36 @@
 (function($){
     
     $('document').ready(function(){
-    
+        
+        // banner home
         $('.home-slider ul').slick({
             arrows: false,
             dots: true,
             autoplay: true,
+            autoplaySpeed: 5000,
             lazyLoad: 'ondemand'
         });
         
+        // pagina sobre
+        $('.slider-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            autoplay: true,
+            lazyLoad: 'ondemand',
+            asNavFor: '.slider-nav'
+        });
+        
+        
+        $('.slider-nav').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for',
+            centerMode: true,
+            focusOnSelect: true
+        });
+    
     });
     
 })(jQuery);
